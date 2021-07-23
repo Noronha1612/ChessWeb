b18d59a65662894874452b071551794f997756d4
NoronhaGCP <inc.691@gmail.com>
2021-07-23 15:02:46 -0300

    update global styles

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -4,8 +4,15 @@ export const GlobalStyles = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-    outline: 0;
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
+  }
+
+  button, a {
+    outline: 0;
+    border: 0;
+    text-decoration: none;
+    cursor: pointer;
+    background: none;
   }
 `;
